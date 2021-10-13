2dfcb8617ea8f282da7065fa69469697fce51ac9
Sagar Chauhan <coolsagar005@gmail.com>
2021-10-13 15:20:13 +0530

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,36 @@
+<script>
+import { defineComponent } from 'vue';
+import SimpleShimmer from '@/simple-shimmer.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    SimpleShimmer
+  }
+});
+</script>
+
+<template>
+  <div id="app" style="margin: 50px;">
+
+    <h3>Simple shimmer block</h3>
+    <simple-shimmer :style="{height:'100px', borderRadius:'10px'}"/>
+
+    <h3>Circular shimmer block</h3>
+    <simple-shimmer :style="{height:'100px', width:'100px', borderRadius:'100%'}"/>
+
+    <h3>Lines shimmer block</h3>
+    <simple-shimmer :style="{height:'10px', width:'auto', borderRadius:'8px'}"/>
+    <simple-shimmer :style="{height:'10px', width:'auto', borderRadius:'8px'}"/>
+    <simple-shimmer :style="{height:'10px', width:'auto', borderRadius:'8px'}"/>
+    <simple-shimmer :style="{height:'10px', width:'auto', borderRadius:'8px'}"/>
+
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+#app{
+  font-family: Nunito,serif;
+}
+</style>
---
+++ src/simple-shimmer.vue
@@ -0,0 +1,40 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default /*#__PURE__*/defineComponent({
+  name: 'SimpleShimmer', // vue component name
+});
+</script>
+
+<template>
+  <div class="shimmer animate" style="{styleData}"></div>
+</template>
+
+<style scoped>
+.shimmer {
+  height: 100px;
+  background: #777;
+  margin-top: 20px;
+}
+
+@keyframes fullView {
+  100% {
+    width: 100%;
+  }
+}
+
+.animate {
+  animation : shimmer 2s infinite linear;
+  background: linear-gradient(to right, #eff1f3 4%, #e2e2e2 25%, #eff1f3 36%);
+  background-size: 1000px 100%;
+}
+
+@keyframes shimmer {
+  0% {
+    background-position: -1000px 0;
+  }
+  100% {
+    background-position: 1000px 0;
+  }
+}
+</style>
